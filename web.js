624ff0fb976e883f797bd50f1a42af1c28f06be4
Jose Corte-Real <mlashcorp@gmail.com>
2013-07-10 21:11:57 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -5,8 +5,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send(b.toString());
+});
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-
 });
