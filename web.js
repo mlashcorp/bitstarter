916bc3d65690fe18793338f1c6836f402343f94d
Jose Corte-Real <mlashcorp@gmail.com>
2013-07-10 20:56:29 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
-var fs = require('fs');
-var buffer = fs.readFileSync('index.html');
+
+var buffer = fs.readFileSync('index.html','utf8');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+  response.send(buffer);
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
