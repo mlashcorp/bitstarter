3e5ee464bd2bd5e3f007e4a68d6f200db03d7933
Max Faraday <mlashcorp@gmail.com>
2013-08-18 13:53:33 +0100

    updated to production version

--- web.js
+++ web.js
@@ -3,10 +3,17 @@ var fs = require('fs');
 var b = fs.readFileSync('index.html');
 var app = express.createServer(express.logger());
 
+app.configure(function () {
+    app.use(
+        "/", //the URL throught which you want to access to you static content
+        express.static(__dirname) //where your static content is located in your filesystem
+    );
+})
+;
 app.get('/', function(request, response) {
   response.send(b.toString());
 });
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
